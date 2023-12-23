79882f6528f9ee1288c182ce85eca48bcad079c2
utkoleg <Mun_2015@bk.ru>
2023-12-23 21:34:57 +0600

    a

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import {BiHeart} from "react-icons/bi";
 import "./Header.css"
-import {Link} from "react-router-dom";
 
 function Header() {
     return (
         <div className="header">
             <div className="header-logo">
-                <h1><a href="/">[utk]rent.</a></h1>
+                <h1>[utk]rent.</h1>
             </div>
             <div className="header-list">
                 <ul>
-                    <li className="list"><BiHeart/></li>
+                    <li><BiHeart/></li>
                     <li>|</li>
-                    <li className="list">Living spaces</li>
+                    <li>Living spaces</li>
                     <li>|</li>
-                    <li className="list">Business solutions</li>
+                    <li>Business solutions</li>
                     <li>|</li>
-                    <li className="list">List of properties</li>
+                    <li>List of properties</li>
                     <li>|</li>
-                    <li className="list"><a className="list" href="/login">Log in</a></li>
+                    <li>Log in</li>
                     <li>|</li>
-                    <li className="list">Sing up</li>
+                    <li>Sing up</li>
                 </ul>
             </div>
         </div>
