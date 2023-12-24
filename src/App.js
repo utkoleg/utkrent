e64d97af71996c728fa2862faf1f37d64553cd0c
utkoleg <Mun_2015@bk.ru>
2023-12-24 20:15:30 +0600

    a

--- src/App.js
+++ src/App.js
@@ -2,25 +2,22 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./components/HomePage/HomePage";
-import Sidebar from "./components/Sidebar/Sidebar";
-import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-    <Header/>
-      <BrowserRouter>
-        <Sidebar>
-          <Routes>
-            <Route path="/" element={<HomePage/>}></Route>
-          </Routes>
-        </Sidebar>
-      </BrowserRouter>
-      <HomePage/>
-      <Footer/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}></Route>
+                </Routes>
+            </BrowserRouter>
+            <HomePage/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
