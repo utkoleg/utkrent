98b03f428f7a2377bc8921ba864a69e3d89547e2
utkoleg <Mun_2015@bk.ru>
2023-12-24 20:03:58 +0600

    a

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./components/HomePage/HomePage";
+import Sidebar from "./components/Sidebar/Sidebar";
+import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
     <Header/>
+      <BrowserRouter>
+        <Sidebar>
+          <Routes>
+            <Route path="/" element={<HomePage/>}></Route>
+          </Routes>
+        </Sidebar>
+      </BrowserRouter>
       <HomePage/>
       <Footer/>
     </div>
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+import {IoMdHome} from "react-icons/io";
+import {BiHeart} from "react-icons/bi";
+import {MdBusinessCenter, MdOutlineHomeWork} from "react-icons/md";
+import {CiLogin} from "react-icons/ci";
+import {NavLink} from "react-router-dom";
+import {FaBars} from "react-icons/fa";
+import "./Sidebar.css"
+
+function Sidebar() {
+    const [isOpen, setIsOpen] = useState(false)
+    const toggle = () => setIsOpen(!isOpen)
+    const menuItem = [
+        {
+            name: "Home",
+            icon: <IoMdHome/>,
+            path: "/"
+        },
+        {
+            name: "Favorites",
+            icon: <BiHeart/>,
+            path: "/favourites"
+        },
+        {
+            name: "Living spaces",
+            icon: <MdOutlineHomeWork/>,
+            path: "/living-spaces"
+        },
+        {
+            name: "Business solutions",
+            icon: <MdBusinessCenter/>,
+            path: "/business-solutions"
+        },
+        {
+            name: "Log in",
+            icon: <CiLogin/>,
+            path: "/log-in"
+        }
+    ]
+    return (
+        <div className="containerr">
+            <div style={{width: isOpen ? "200px" : "0"}} className="sidebar">
+                <div className="top_section">
+                    <div style={{marginLeft: isOpen ? "150px" : "0px"}} className="bars">
+                        <FaBars onClick={toggle}/>
+                    </div >
+                </div>
+                {
+                    menuItem.map((item, index) => (
+                        <NavLink to={item.path} key={index} className="link" activeclassName="active" style={{display: isOpen ? "block" : "none"}}>
+                            <div className="icon">{item.icon}</div>
+                            <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
+                        </NavLink>
+                    ))
+                }
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar;
