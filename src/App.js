584b00b55a7db0cfda9440cdb8927518336960e2
peacedauren <abilkassym.d@bk.ru>
2023-12-24 22:28:23 +0600

    log in

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./components/HomePage/HomePage";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import React from "react";
+import AddImgModal from './components/AddImgModal/AddImgModal';
 
 function App() {
+    const [selectedCard, setSelectedCard] = React.useState(null)
+  
+  const closePopup = () => {
+    setSelectedCard(null)
+  }
     return (
         <div className="App">
             <Header/>
-            <BrowserRouter>
+            <Router>
                 <Routes>
                     <Route path="/" element={<HomePage/>}></Route>
+                    <Route path="/login" element={<AddImgModal />}/>
                 </Routes>
-            </BrowserRouter>
+            </Router>
             <HomePage/>
             <Footer/>
         </div>
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -7,84 +7,88 @@ import GooglePlay from "./img/google-play-store-icon.png"
 import Flat1 from "./img/flats/flat.jpg"
 import Flat2 from "./img/flats/flat2.jpg"
 import Flat3 from "./img/flats/flat3.jpg"
+import AddImgModal from '../AddImgModal/AddImgModal';
 
 
 function HomePage() {
     return (
+        <>
         <div>
 
-            {/*ПОИСКОВАЯ СТРОКА*/}
-            <div className="home-page-banner">
-                <h1>Find your dream place</h1>
-                <div className="searchbar">
-                    <input placeholder="Input your desired city"/>
-                    <button>Search</button>
-                </div>
-            </div>
+{/*ПОИСКОВАЯ СТРОКА*/}
+<div className="home-page-banner">
+    <h1>Find your dream place</h1>
+    <div className="searchbar">
+        <input placeholder="Input your desired city"/>
+        <button>Search</button>
+    </div>
+</div>
 
-            {/*МЕДИА*/}
-            <div className="home-page-app">
-                <div className="image-div">
-                    <img src={HandImage} alt="hand"/>
-                </div>
-                <div className="image-text">
-                    <div style={{width: "80%"}}>
-                        <h1>The <span style={{color: "royalblue"}}> fastest</span> way to rent a home is with the app.
-                        </h1>
-                        <h2>Now with the app saving your favorite properties is easier than ever. Available now in the
-                            Apple
-                            App Store and Google Play App Store.</h2>
-                        <div className="qr">
-                            <img src={Qr} alt="qr code"/>
-                            <div className="qr-icons">
-                                <img src={Appstore} alt="appstore"/>
-                                <img src={GooglePlay} alt="googleplay" style={{marginTop: "10px"}}/>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="home-page-bot">
-                <div className="dodots">
-                    <div className="dots">
-                        <h1>Best solutions for <span style={{color: "royalblue"}}> you</span></h1>
-                        <h2>lorem ipsum lorem ipsum lorem ipsum </h2>
-                        <button>See offers</button>
-                    </div>
+{/*МЕДИА*/}
+<div className="home-page-app">
+    <div className="image-div">
+        <img src={HandImage} alt="hand"/>
+    </div>
+    <div className="image-text">
+        <div style={{width: "80%"}}>
+            <h1>The <span style={{color: "royalblue"}}> fastest</span> way to rent a home is with the app.
+            </h1>
+            <h2>Now with the app saving your favorite properties is easier than ever. Available now in the
+                Apple
+                App Store and Google Play App Store.</h2>
+            <div className="qr">
+                <img src={Qr} alt="qr code"/>
+                <div className="qr-icons">
+                    <img src={Appstore} alt="appstore"/>
+                    <img src={GooglePlay} alt="googleplay" style={{marginTop: "10px"}}/>
                 </div>
 
-                <div className="bot-menu">
-                    <div className="bot-menu-element">
-                        <img src={Flat1} alt="flat1"/>
-                        <div className="bot-menu-text">
-                            <h1>Almaty, Al-Farabi 9</h1>
-                            <h2>Самый крутой домешник спору нет</h2>
-                            <button>See more</button>
-                        </div>
-                    </div>
-
-                    <div className="bot-menu-element">
-                        <img src={Flat2} alt="fla2"/>
-                        <div className="bot-menu-text">
-                            <h1>Shymkent, Nursat 1</h1>
-                            <h2>Ну нурсат спорная фигня</h2>
-                            <button>See more</button>
-                        </div>
-                    </div>
-
-                    <div className="bot-menu-element">
-                        <img src={Flat3} alt="flat3"/>
-                        <div className="bot-menu-text">
-                            <h1>Astana, Mangilik el 19</h1>
-                            <h2>Астана для лошков</h2>
-                            <button>See more</button>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
+    </div>
+</div>
+
+<div className="home-page-bot">
+    <div className="dodots">
+        <div className="dots">
+            <h1>Best solutions for <span style={{color: "royalblue"}}> you</span></h1>
+            <h2>lorem ipsum lorem ipsum lorem ipsum </h2>
+            <button>See offers</button>
+        </div>
+    </div>
+
+    <div className="bot-menu">
+        <div className="bot-menu-element">
+            <img src={Flat1} alt="flat1"/>
+            <div className="bot-menu-text">
+                <h1>Almaty, Al-Farabi 9</h1>
+                <h2>Самый крутой домешник спору нет</h2>
+                <button>See more</button>
+            </div>
+        </div>
+
+        <div className="bot-menu-element">
+            <img src={Flat2} alt="fla2"/>
+            <div className="bot-menu-text">
+                <h1>Shymkent, Nursat 1</h1>
+                <h2>Ну нурсат спорная фигня</h2>
+                <button>See more</button>
+            </div>
+        </div>
+
+        <div className="bot-menu-element">
+            <img src={Flat3} alt="flat3"/>
+            <div className="bot-menu-text">
+                <h1>Astana, Mangilik el 19</h1>
+                <h2>Астана для лошков</h2>
+                <button>See more</button>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+{/* <AddImgModal isAddActive={true}/> */}
+        </>
     );
 }
 
